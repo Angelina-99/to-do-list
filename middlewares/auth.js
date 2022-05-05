efc8a06413794d7703c00fa6a7192b9c6b2192b1
Angelina-99 <Angelinaparenkova@gmail.com>
2022-05-05 11:01:33 +0300

    removed unnecessary from auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// router.post("/auth", async (req, res) => {
-//     if (!req.body || !req.body.login || !req.body.password) {
-//         return res.status(401).end();
-//     }
-//     const query = 'SELECT * FROM tasks;';
-//     try {
-//         const result = await req.db.query(query);
-//         res.json(result.rows)
-//     } catch (err) {
-//         res.json({err: err})
-//     }
-// });
-
-// module.exports = router;
-
 const jwt = require("jsonwebtoken");
 
 router.use((req, res, next) => {
